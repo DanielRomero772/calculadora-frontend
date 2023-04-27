c93186b595edc2823c76f06dd6e5a865cdfc1227
Cristofer-IA <cristofer0993@gmail.com>
2023-04-27 17:43:30 -0500

    calculadora-v6.4

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,42 @@ import Calculadora from './page/Calculadora';
 import Home from './page/Home';
 import Nav from './page/Nav';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getAllIngredientes } from './store/slices/ingredientes.slice';
 import { getAllUme } from './store/slices/ume.slice';
 import Footer from './page/Footer';
 import './App.css';
 
 function App() {
+  const ume = useSelector(state => state.ume);
+  //const ume = null;
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getAllIngredientes());
     dispatch(getAllUme());
   }, []);
-  return (
-    <div className="App">
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route element={<ProtectRouter />}>
-          <Route path={'/calculadora'} element={<Calculadora />} />
-        </Route>
-      </Routes>
-      <Footer />
-    </div>
-  );
+
+  if (ume) {
+    return (
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route element={<ProtectRouter />}>
+            <Route path={'/calculadora'} element={<Calculadora />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </div>
+    );
+  } else {
+    return (
+      <div className="load">
+        <span class="loader"></span>
+      </div>
+    );
+  }
 }
 
 export default App;
